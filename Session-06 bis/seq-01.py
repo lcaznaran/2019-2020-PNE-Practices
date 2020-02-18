0bf177f7c254eb0da7949b3eb840ae904d7ecf7c
aznaranl <lc.aznaran.2019@alumnos.urjc.es>
2020-02-18 15:36:27 +0100

    Session 5

---
+++ Session-06 bis/seq-01.py
@@ -0,0 +1,11 @@
+class Seq:
+    """A class for representing sequence objects"""
+    def __init__(self):
+        print("New sequence created")
+
+#--Main program
+s1 = Seq()
+s2 = Seq()
+
+print("Testing objects...")
+
