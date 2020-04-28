e80e5a04ddd5a4c6c1fb4fc0143578c989b26958
lcaznaran <lc.aznaran.2019@alumnos.urjc.es>
2020-04-28 18:09:01 +0200

    Session 10

--- P3/Seq-Server.py
+++ P3/Seq-Server.py
@@ -1,6 +1,8 @@
 import socket
 import termcolor
+from Seq1 import Seq
 
+seq = ["ACTGAACTTGACCTACGGTCA","TTCGACCGGAAGTCCAATTTG","CCTAGGAACTTTGACGTAACT","ACGTCAGCTAGTGCTAACGTA"]
 # -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -23,8 +25,37 @@ ls.bind((IP, PORT))
 # -- Step 3: Configure the socket for listening
 ls.listen()
 
+def get_func(n):
+    return seq[n]
+def info_func(s):
+    s=Seq(s)
+    l = s.length()
+    ac = s.count_base("A")
+    tc = s.count_base("T")
+    cc = s.count_base("C")
+    gc = s.count_base("G")
+    resp = f"""Sequence: {s}
+    Total length: {l}
+    A: {ac} ({round((ac/l)*100)})%  
+    C: {cc} ({round((cc/l)*100)})%     
+    T: {tc} ({round((tc/l)*100)})%     
+    G: {gc} ({round((gc/l)*100)})%"""
+    return resp
+
+def comp_func(s):
+    s = Seq(s)
+    compl = s.seq_complement()
+    return compl
+
+def rev_func(s):
+    s = Seq(s)
+    rev = s.seq_reverse()
+    return rev
+
+
 print("SEQ server configured!")
 
+listn=["1","2","3","4"]
 while True:
     # -- Waits for a client to connect
     print("Waiting for clients to connect")
@@ -48,11 +79,35 @@ while True:
         # -- We decode it for converting it
         # -- into a human-redeable string
         msg = msg_raw.decode()
+        lines = msg.split("\n")
+        line0 = lines[0].strip()
+        lcmds = line0.split(' ')
+        comm = lcmds[0]
+        try:
+            arg = lcmds[1]
+        except IndexError:
+            # -- No arguments
+            arg = ""
         # -- Print the received message
         print(f"Message received: ", end="")
-        if msg == "PING":
+        response=""
+        if comm == "PING":
             termcolor.cprint("PING command", 'green')
             response = "OK!\n"
-            print(response)
-            cs.send(response.encode())
-            cs.close()
+
+        elif comm == "GET":
+            termcolor.cprint("GET command", 'green')
+            response = get_func(int(arg))+"\n"
+        elif comm == "INFO":
+            termcolor.cprint("INFO command", 'green')
+            response = info_func(arg) + "\n"
+        elif comm == "COMP":
+            termcolor.cprint("COMP command", 'green')
+            response = comp_func(arg) + "\n"
+        elif comm == "REV":
+            termcolor.cprint("REV command", 'green')
+            response = rev_func(arg) + "\n"
+
+        print(response)
+        cs.send(response.encode())
+        cs.close()
